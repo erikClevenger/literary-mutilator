0094aad3dad86912a6838bb88cf69cbfc5cbf3c9
Erik Clevenger <eclevenger@cnm.edu>
2018-05-08 22:04:42 -0600

    logic fuzzy too tired to figure it out

--- scripts.js
+++ scripts.js
@@ -1,8 +1,49 @@
 
 
+/*function clickyText() {
+	var paraWords = 	fetch('http://baseballipsum.apphb.com/api/?paras=3')
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(myJson) {
+			console.log(myJson);
+		});
+   return paraWords
+}
+*/
 
 
+function createNode(element) {
+	return document.createElement(element);
+}
 
+function append(parent, el) {
+	return parent.appendChild(el);
+}
+
+const paras = document.getElementById('newParas');
+const url = 'http://baseballipsum.apphb.com/api/?paras=3';
+var data = {int:'example'};
+
+function paraData(url,data) {
+	fetch(url)
+		.then((resp) => resp.json())
+		.then(function(data) {
+			let paragraphs = data.results;
+			return paragraphs.map(function(paragraphs) {
+				let p = createNode('p');
+				append(paras, p);
+				append(paras, p);
+				append(paras, p);
+			})
+		})
+		.catch(function(error) {
+			console.log(JSON.stringify(error));
+		});
+
+paraData(url,data);
+
+/*
 // takes a string to split into an array of words.
 function splitString(stringToSplit, separator) {
 	var arrayOfStrings = stringToSplit.split(separator);
@@ -18,7 +59,7 @@ var paraText = splitString(paraContent, space);
 var item = paraText[Math.floor(Math.random()*paraText.length)];
 
 console.log(item);
-
+*/
 //todo refactor the above mess
 
 
